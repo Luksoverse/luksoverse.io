7a55ff0eb60dc56ad29d849302d8ebb1f9d077d2
JordyDutch <jordydutch1@gmail.com>
2023-10-13 15:52:45 +0200

    add analytics

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
-import Alert from './alert'
-import Footer from './footer'
-import Meta from './meta'
+import Alert from "./alert";
+import Footer from "./footer";
+import Meta from "./meta";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Layout({ preview, children }) {
   return (
@@ -9,8 +10,9 @@ export default function Layout({ preview, children }) {
       <div className="min-h-screen">
         <Alert preview={preview} />
         <main>{children}</main>
+        <Analytics />
       </div>
       <Footer />
     </>
-  )
+  );
 }
